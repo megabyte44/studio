2dca4e6f0d31b1e36a12f34f4db731eb67a99351
Punith Medaramitta <punithmedaramitta@gmail.com>
2025-06-22 04:55:20 +0000

    bring water intake to top

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -320,11 +320,13 @@ export default function DashboardPage() {
           )}
         </header>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+            <div className="lg:col-span-3">
+                <WaterIntakeWidget />
+            </div>
             <div className="lg:col-span-2">
                 <TodaysPlan />
             </div>
             <div className="lg:col-span-1 space-y-6">
-                <WaterIntakeWidget />
                 <FinancialSnapshot />
                 <Alarm />
             </div>
