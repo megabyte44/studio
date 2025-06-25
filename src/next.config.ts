bace1cf0d8299fe80cf309c9e7a49bea16a1acf1
Punith Medaramitta <punithmedaramitta@gmail.com>
2025-06-25 19:42:04 +0000

    Disable Dev Tools for this project To disable this UI completely, set de

---
+++ src/next.config.ts
@@ -0,0 +1,34 @@
+import type {NextConfig} from 'next';
+import withPWAInit from '@ducanh2912/next-pwa';
+
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  devIndicators: {
+    buildActivity: false,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default withPWA(nextConfig);
